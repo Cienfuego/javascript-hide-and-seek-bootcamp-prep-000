8e567370b9a19a57961be7152e88a10d279f6128
Michael Tompkins <mtompkins82@gmail.com>
2018-08-19 13:05:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function increaseRankBy(n){
 }
 
 function deepestChild(){
-  return console.log(document.getElementById('grand-node').querySelectorAll('div'))
+  return document.getElementById('grand-node').querySelectorAll('div'))
 }
