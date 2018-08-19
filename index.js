3bbe1f686c8e1b1f4bbe372fa55d2fa4e0145269
Michael Tompkins <mtompkins82@gmail.com>
2018-08-19 13:11:11 +0000

    Done.

--- index.js
+++ index.js
@@ -5,3 +5,14 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.getElementById("nested").querySelector(".target");
 }
+
+function increaseRankBy(n){
+  var rank = document.querySelectorAll('ul.ranked-list li');
+	for (let i = 0; i < rank.length; i++){
+    rank[i].innerHTML = parseInt(rank[i].innerHTML)+n
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div');
+}
